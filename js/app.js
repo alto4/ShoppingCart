3160d205a9ece229ab0d1c2085e46394c381f988
Scott Alton <scottaltonmusic@gmail.com>
2020-07-02 01:29:39 -0400

    Calculate total price and input fields added.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,55 @@
   var updateSubtotal = function (el) {
-    var quantity = parseFloat($(el).find('.quantity input').val());
-    var unitPrice = parseFloat($(el).children('.unitPrice').text());
+    var quantity = Number($(el).find('.quantity input').val());
+    var unitPrice = Number($(el).children('.unitPrice').text());
     // Calculate subtotal
     var subTotal = quantity * unitPrice;
-    subTotal = subTotal + '.00';
-    $(el).children('.subtotal').html('$' + subTotal);
+    subTotal = subTotal;
+    $(el).children('.subtotal').html('$' + subTotal + '.00');
 
     return subTotal;
   }
 
   // 2. Calculate and display the total price.
-  $('.subtotal').each(function (el) {
-    console.log($(el).text());
-  });
-  // 3. Allow user to add a new item.
+  var updateTotals = function () {
+    var sum = function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    };
+    var itemSubtotals = [];
+    // 1. Loop through each item in cart and calculate subtotal for each item
+    $('tbody tr').each(function (i, el) {
+      var subtotal = updateSubtotal(el);
+      itemSubtotals.push(subtotal);
+      console.log(itemSubtotals)
+    });
 
-  // 4. Allow user to delete an item.
+    var total = itemSubtotals.reduce(sum);
+    $('.totalPrice').html('$' + total + '.00');
+  }
 
   // 5. Use Git to manage version control and push your work to a GitHub repository.
 
-  // On DOM content loaded
+  // Once all DOM elements are ready, add functionality 
   $(document).ready(function () {
+    updateTotals();
 
-    // 1. Loop through each item in cart and calculate subtotal for each item
-    $('tbody tr').each(function (i, el) {
-      updateSubtotal(el);
+    // Update item subtotal and overall total when calculate button is clicked
+    $('.btn-calculate').on('click', function (e) {
+      e.preventDefault();
+      updateTotals();
     });
-  })
+
+    ////////////////////RESUME BELOW WITH ADD NEW ITEM
+    // Add item
+    $('#addItem').on('submit', function (e) {
+      console.log('submitted new item');
+      e.preventDefault();
+      var item = $(this).find('[name=item]').val();
+      console.log(item);
+    })
+
+    // Allow user to delete an item.
+    $('.btn-remove').on('click', function () {
+      $(this).closest('tr').remove();
+      updateTotals();
+    });
+  });
